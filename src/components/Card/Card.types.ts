0d27125e5f0b75865aaa360274040f8b0c2d9147
Матвей Калашников <kmgpmain@gmail.com>
2024-08-14 20:25:43 +0700

    pr(): декларация типа пропсов для провайдера вынесена в файл типов

--- src/components/Card/Card.types.ts
+++ src/components/Card/Card.types.ts
@@ -1,5 +1,6 @@
 import { ContactsInfo } from '../../types/entities/Contacts.ts';
 import { ProjectInfo } from '../../types/entities/Project.ts';
+import { Dispatch, PropsWithChildren, SetStateAction } from 'react';
 
 export type Forms = {
   contacts: ContactsInfo;
@@ -10,3 +11,8 @@ export type TabType = {
   label: string;
   component: JSX.Element;
 };
+
+export type IsFormActiveProviderProps = {
+  isActive: boolean;
+  setIsActive: Dispatch<SetStateAction<boolean>>;
+} & PropsWithChildren;
