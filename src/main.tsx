921f79a3cdb6e7580edcff1d52f26aa1c9b4fd5c
Матвей Калашников <kmgpmain@gmail.com>
2024-08-14 20:00:36 +0700

    pr(): убрал ненужный код из main.tsx и перенес его в App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { theme } from './theme.ts';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <CssBaseline>
-      <ThemeProvider theme={theme}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <App />
-        </LocalizationProvider>
-      </ThemeProvider>
-    </CssBaseline>
-  </React.StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
